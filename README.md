79f3a3311de6251a5e6282488ef1e604317db24f
Leonardo Yzquierdo <leoyzquierdo@gmail.com>
2022-03-04 20:13:05 -0300

    completed the challenge and created README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Rocketseat Ignite (ReactJS) - To-do List solution
+
+This is a solution to the to-do list challenge of Rocketseat Ignite
+### ⚔️ The challenge
+
+Users should be able to:
+
+- Create a new task
+- Remove a task
+- Mark and unmark a task as complete
+
+## 👀 Preview
+
+![](./readme-files/preview.gif)
+
+### 🔨 Built with
+
+- <strong>ReactJS</strong>
+- <strong>Typescript</strong>
+- Semantic <strong>HTML5</strong> markup
+- <strong>CSS</strong> custom properties
+- Flexbox
+- <strong>Sass</strong>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -12,18 +12,37 @@ interface Task {
 
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === "") return
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(oldState => [...oldState, newTask])
+    setNewTaskTitle("")
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const completedTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task) 
+
+    setTasks(completedTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id) 
+
+    setTasks(filteredTasks)
   }
 
   return (
@@ -66,7 +85,6 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
         </ul>
       </main>
     </section>
